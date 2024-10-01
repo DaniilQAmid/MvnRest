17efbc1749740586490ddcc862d0a707b1666727
Daniil <tornado9815@mail.ru>
2024-10-01 16:00:34 +0300

    Fix services

--- src/main/java/ru/netology/javaqa/freelanceRest/services/rest.java
+++
@@ -1,8 +0,0 @@
-package ru.netology.javaqa.freelanceRest.services;
-
-public class rest {
-    public static void main(String[] args){
-        restService service = new restService();
-        System.out.println("Количество месяцев отдыха в следующем году: " + service.calculate(10_000, 3_000, 20_000));
-    }
-}
--- src/main/java/ru/netology/javaqa/freelanceRest/services/restService.java
+++ src/main/java/ru/netology/javaqa/freelanceRest/services/restService.java
@@ -4,11 +4,11 @@ public class restService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
-        for (int mount = 1; mount < 12; mount++) {
+        for (int i = 0; i < 12; i++) {
             if (money >= threshold) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
-                int entertainments = money / 3; // дополнительные траты на отдыхе
-                money = money - expenses - entertainments;
+                count++; // увеличиваем счётчик месяцев отдых
+                money = money - expenses; // дополнительные траты на отдыхе
+                money = money / 3;
             } else {
                 money = money + income - expenses;
             }
